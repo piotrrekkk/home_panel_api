df0e8b42ff6e4c99032a2b8b958efc912a9a1a47
Piotr Rosa <Piotr.Rosa.ctr@sabre.com>
2016-11-13 22:14:25 +0100

    response visible on html page

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 (function() {
     $.get('/dht11', function(res) {
-        if(res.temperature) {
+        if (res.temperature) {
             $('#temp').html(res.temperature) + 'st.C';
         }
         $('#hum').html(res.humidity) + '%';
