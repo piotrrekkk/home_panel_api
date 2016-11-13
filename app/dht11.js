11c6a7c487453e8838a4a15a6995409977e9a620
Piotr Rosa <Piotr.Rosa.ctr@sabre.com>
2016-11-13 21:35:31 +0100

    temperature impl

--- app/dht11.js
+++ app/dht11.js
@@ -3,13 +3,9 @@ var dht = require('dht-sensor');
 exports.getActualData = function() {
     var current = dht.read(11, 4);
 
-    if (current.temperature !== 0) {
-        return {
-            'temperature': current.temperature,
-            'humidity': current.humidity
-        }
-    } else {
-        return 'Invalid data'
+    return {
+        'temperature': current.temperature,
+        'humidity': current.humidity
     }
 }
 
