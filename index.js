1b19f702dee3217bbdd615847e9c43783eeb6df3
Piotr Rosa <Piotr.Rosa.ctr@sabre.com>
2016-11-13 19:56:31 +0100

    body-parser added

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/dht11', function(req, res) {
+var router = app.Router();
+
+router.get('/dht11', function(req, res) {
     res.json(dht11.data);
 })
 
-app.get('/test', function(req, res) {
+router.get('/test', function(req, res) {
     res.json(dht11.sampleData);
 })
 
-app.listen(8081, function() {
+router.listen(8081, function() {
     console.log('Listening on 8081');
 })
